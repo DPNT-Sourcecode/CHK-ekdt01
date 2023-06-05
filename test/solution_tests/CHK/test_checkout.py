30a1457000a077993c1a870aa2a7a56d01cfcc7a
sbx_user1051 <sbx_user1051@169.254.20.149>
2023-06-05 08:08:29 +0000

    Mon Jun 05 08:08:29 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -11,4 +11,15 @@ class TestCheckout():
         assert checkout('D') == 15
         assert checkout('E') == 40
 
+    def test_multiple_items(self):
+        assert checkout('AAA') == 130
+        assert checkout('AAAAA') == 200
+        assert checkout('AAAAAAAAA') == 380
+        assert checkout('CCCC') == 80
+        assert checkout('DD') == 30
+        assert checkout('EEEE') == 120
+
+    def test_invalid(self):
+        assert checkout('Z') == -1
+
         
