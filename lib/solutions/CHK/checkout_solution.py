e965e6f58b78b8bdb0d2436276054069eab7b07e
sbx_user1051 <sbx_user1051@169.254.20.149>
2023-06-05 07:49:17 +0000

    Mon Jun 05 07:49:17 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -20,9 +20,16 @@ class Checkout:
 
         self.special_offers = {
             'A':[(5, 200), (3, 130)],
-            'B':(2, 45),
-            'E':(3, 80)
+            'B':[(2, 45)],
+            'E':[(3, 80)]
         }
+
+    def calculate_special(self, count, special):
+        special_amount, special_price = special
+        special_count = math.floor(count / special_amount)
+        remaining = count % special_amount
+        total_price = special_price * special_count
+        return total_price, remaining
 
     def calculate_total_price(self, order):
         if not isinstance(order, str):
@@ -39,16 +46,23 @@ class Checkout:
 
             # if on special offer we need to keep track of the number of items and check if special offer needed
             item_count[item] = 1 if item not in item_count else item_count[item] + 1
-            special_offer_amount = self.special_offers[item][0]
-            special_offer_price = self.special_offers[item][1]
+            # special_offer_amount = self.special_offers[item][0]
+            # special_offer_price = self.special_offers[item][1]
 
-            # special offer needed
-            if item_count[item] != 0 and item_count[item] % special_offer_amount == 0:
-                total_price += special_offer_price - ( special_offer_amount - 1 ) * self.prices[item]
-                continue
+            # # special offer needed
+            # if item_count[item] != 0 and item_count[item] % special_offer_amount == 0:
+            #     total_price += special_offer_price - ( special_offer_amount - 1 ) * self.prices[item]
+            #     continue
 
-            # special offer not needed yet
-            total_price += self.prices[item]    
+            # # special offer not needed yet
+            # total_price += self.prices[item]    
+        
+        for item in self.special_offers:
+            specials = self.special_offers[item]
+            for offer in specials:
+                total_price, remaining = self.calculate_special(item_count[item], offer)
+                item_count[item] = remaining
+
 
         return total_price
 
@@ -57,9 +71,10 @@ class Checkout:
         
 # checkout = Checkout()
 
-# print(checkout('AAAABBBCCDD') )
+print(checkout('AAAABBBCCDD') )
     
 
     
         
+
 
